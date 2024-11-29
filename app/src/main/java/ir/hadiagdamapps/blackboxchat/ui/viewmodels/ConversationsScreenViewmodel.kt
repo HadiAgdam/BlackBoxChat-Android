089c552334c9ad73f7b641c9d9c662dbb4c44ace
HadiAgdam <hadiagdamdeveloper@gmail.com>
2024-11-29 18:51:06 +0330

    added MessageReceiver to viewmodel

--- app/src/main/java/ir/hadiagdamapps/blackboxchat/ui/viewmodels/ConversationsScreenViewmodel.kt
+++ app/src/main/java/ir/hadiagdamapps/blackboxchat/ui/viewmodels/ConversationsScreenViewmodel.kt
@@ -10,25 +10,54 @@ import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.core.text.isDigitsOnly
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import androidx.navigation.NavController
 import ir.hadiagdamapps.blackboxchat.data.Clipboard
 import ir.hadiagdamapps.blackboxchat.data.ConversationHandler
+import ir.hadiagdamapps.blackboxchat.data.InboxHandler
+import ir.hadiagdamapps.blackboxchat.data.models.ConnectionStatus
 import ir.hadiagdamapps.blackboxchat.data.models.Label
 import ir.hadiagdamapps.blackboxchat.data.models.Pin
 import ir.hadiagdamapps.blackboxchat.data.models.PublicKey
 import ir.hadiagdamapps.blackboxchat.data.models.conversation.ConversationModel
+import ir.hadiagdamapps.blackboxchat.data.models.message.LocalMessage
+import ir.hadiagdamapps.blackboxchat.data.network.MessageReceiver
 import ir.hadiagdamapps.blackboxchat.data.qr.QrCodeGenerator
 import ir.hadiagdamapps.blackboxchat.ui.navigation.routes.ConversationsRoute
+import kotlinx.coroutines.launch
 
 class ConversationsScreenViewmodel(
     context: Context, private val navController: NavController, private val args: ConversationsRoute
 ) : ViewModel() {
 
+    private var pin: Pin? = null
     private val conversationHandler = ConversationHandler(context)
     private val qrCodeGenerator = QrCodeGenerator()
     private val clipboard = Clipboard(context)
+    private val inbox = InboxHandler(context).getInboxById(args.inboxId)
+    private val messageReceiver = object : MessageReceiver(
+        context = context,
+        inboxId = inbox.inboxId,
+        inboxPrivateKey = inbox.inboxPrivateKey,
+        inboxPin = pin,
+        salt = inbox.salt
+    ) {
+        override fun newConversation(conversationModel: ConversationModel) {
+            _conversations.add(conversationModel)
+        }
 
-    private var pin: Pin? = null
+        override fun onReceived(message: LocalMessage) {
+            _conversations.indexOfFirst { it.conversationId == message.conversationId }
+                .takeIf { it != -1 }?.let {
+                    _conversations[it] = _conversations[it].copy(hasNewMessage = true)
+                }
+        }
+
+        override fun connectionStatusChanged(connectionStatus: ConnectionStatus) {
+            // TODO("Not yet implemented")
+        }
+    }
+
 
     private val _conversations = mutableStateListOf<ConversationModel>()
     val conversations: SnapshotStateList<ConversationModel> = _conversations
@@ -133,6 +162,9 @@ class ConversationsScreenViewmodel(
                     _conversations.add(it)
                 }
 
+                messageReceiver.setPin(pin!!)
+                startPolling()
+
             } catch (ex: Exception) {
                 Log.e("backstack", ex.toString())
                 navController.popBackStack()
@@ -178,8 +210,10 @@ class ConversationsScreenViewmodel(
 
     //----------------------------------------------------------------------------------------------
 
-
-    // TODO poll updates
-    // I am thinking about making a class handling almost everything and just calling updates
+    private fun startPolling() {
+        viewModelScope.launch {
+            messageReceiver.startPolling()
+        }
+    }
 
 }
