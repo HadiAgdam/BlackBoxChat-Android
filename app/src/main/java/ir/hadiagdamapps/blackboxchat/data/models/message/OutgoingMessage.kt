542240b2850da99229bbbf24af862b54b2f517fb
HadiAgdam <hadiagdamdeveloper@gmail.com>
2024-12-17 19:37:09 +0330

    created outGoingMessageModel

---
+++ app/src/main/java/ir/hadiagdamapps/blackboxchat/data/models/message/OutgoingMessage.kt
@@ -0,0 +1,19 @@
+package ir.hadiagdamapps.blackboxchat.data.models.message
+
+data class OutgoingMessage(
+    val receiver: String,
+    val encryptionKey: String,
+    val iv: String,
+    val encryptedMessage: String
+)
+
+//{
+//	"receiver": (receiver's public key),
+//	"encryptionKey": (encrypted using E2EE),
+//	"iv": (for AES encryption),
+//	"encryptedMessage":
+//	 (encrypted using AES) {
+//		 "sender": (senders public key),
+//		 "message": (message text),
+//	},
+//}
