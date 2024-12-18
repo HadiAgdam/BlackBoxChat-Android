40257ad7f666b59505c3958d0a1a3350a6c5a5e0
HadiAgdam <hadiagdamdeveloper@gmail.com>
2024-12-18 22:00:54 +0330

    moved ChatBox out of bottomSheet

--- app/src/main/java/ir/hadiagdamapps/blackboxchat/ui/screens/ChatScreen.kt
+++ app/src/main/java/ir/hadiagdamapps/blackboxchat/ui/screens/ChatScreen.kt
@@ -1,12 +1,13 @@
 package ir.hadiagdamapps.blackboxchat.ui.screens
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.remember
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.Modifier
 import ir.hadiagdamapps.blackboxchat.ui.components.Screen
 import ir.hadiagdamapps.blackboxchat.ui.components.chat.ChatBox
 import ir.hadiagdamapps.blackboxchat.ui.components.chat.ChatItem
@@ -24,20 +25,27 @@ fun ChatScreen(viewmodel: ChatScreenViewmodel) {
     }
 
     BlackBoxChatAndroidTheme {
-        Screen(title = viewmodel.screenTitle, bottomSheet = {
-            ChatBox(
-                text = viewmodel.chatBoxContent,
-                onValueChange = viewmodel::chatBoxValueChange,
-                sendClick = viewmodel::chatBoxSendClick
-            )
-        }) {
-            LazyColumn(state = state) {
-                items(viewmodel.localMessages) {
-                    ChatItem(text = it.text, sent = it.sent)
+        Screen(
+            title = viewmodel.screenTitle
+        ) {
+            Column(modifier = Modifier.fillMaxSize()) {
+                LazyColumn(state = state, modifier = Modifier.weight(1f)) {
+                    items(viewmodel.localMessages) {
+                        ChatItem(text = it.text, sent = it.sent)
+                    }
+                    items(viewmodel.pendingMessages) {
+                        PendingChatItem(text = it.text) {
+                            viewmodel.cancelSendingPendingMessageClick(
+                                it.pendingMessageId
+                            )
+                        }
+                    }
                 }
-                items(viewmodel.pendingMessages) {
-                    PendingChatItem(text = it.text) { viewmodel.cancelSendingPendingMessageClick(it.pendingMessageId) }
-                }
+                ChatBox(
+                    text = viewmodel.chatBoxContent,
+                    onValueChange = viewmodel::chatBoxValueChange,
+                    sendClick = viewmodel::chatBoxSendClick
+                )
             }
         }
     }
